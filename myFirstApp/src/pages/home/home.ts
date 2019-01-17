ee4e5bf743fa152a96331336815fe2ea7376a2d9
YouqinS <33859677+YouqinS@users.noreply.github.com>
2019-01-17 13:33:13 +0200

    task A

--- myFirstApp/src/pages/home/home.ts
+++ myFirstApp/src/pages/home/home.ts
@@ -26,56 +26,22 @@ import { stringify } from 'querystring';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public http: HttpClient) {
+  constructor(public navCtrl: NavController, private http: HttpClient) {
     this.getImages();
   }
 
 
-/*
-  picArray: Pic[] = [
-    {
-      'title': 'Title 1',
-      'details': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-      'thumbnail': 'http://placekitten.com/310/302',
-      'original': 'http://placekitten.com/2048/1920',
-    },
-    {
-
-      'title': 'Title 2',
-      'details': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-      'thumbnail': 'http://placekitten.com/321/300',
-      'original': 'http://placekitten.com/2041/1922',
-    },
-    {
-      'title': 'Title 3',
-      'details': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-      'thumbnail': 'http://placekitten.com/319/301',
-      'original': 'http://placekitten.com/2039/1920',
-    },
-
-
-  ];
-*/
-
-
-
-
   private url:string = 'assets/test.json';
-
   private picArray;
+
   getImages(){
     this.http.get<Pic[]>(this.url).subscribe((res:any) =>
      // console.log(res));
       this.picArray = res);
 
-
-    //this.picArray = this.http.get<Pic[]>(this.url).subscribe((data: any) => this.picArray = data.data);
     //this.http.get<some_type>('example.json').subscribe((res: some_type) => this.someVariable = res.json());
     console.log('clicked');
-    //console.log(this.picArray);
   }
-
-
 
 
 
