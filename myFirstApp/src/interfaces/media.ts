c8429b7b35a28f35e093099c534cb475448821db
YouqinS <33859677+YouqinS@users.noreply.github.com>
2019-01-17 18:22:52 +0200

    thumbnail

--- myFirstApp/src/interfaces/media.ts
+++ myFirstApp/src/interfaces/media.ts
@@ -9,4 +9,6 @@ export interface Media {
   "media_type": string;
   "mime_type": string;
   "time_added": string;
+
+
 }
--- myFirstApp/src/pages/home/home.ts
+++ myFirstApp/src/pages/home/home.ts
@@ -3,9 +3,6 @@ import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subscription } from 'rxjs';
 import { Media } from '../../interfaces/media';
-import { stringify } from 'querystring';
-//import { PhotoViewer } from '@ionic-native/photo-viewer'; , private photoViewer: PhotoViewer
-
 
 
 
@@ -17,9 +14,12 @@ import { stringify } from 'querystring';
 export class HomePage {
 
   constructor(public navCtrl: NavController, public http: HttpClient) {
-    this.getImages();
+
   }
 
+  ngOnInit(){
+    this.getImages();
+  }
 
 
   //private url:string = 'assets/test.json';
@@ -27,13 +27,21 @@ export class HomePage {
 
   private mediaArray:Media[]=[];
   getImages(){
-    this.http.get<Media[]>(this.url).subscribe((res:any) =>
-    //  console.log(res));
-    this.mediaArray = res);
+    this.http.get<Media[]>(this.url).subscribe((res:Media[]) =>{
+      console.log(res);
+      this.mediaArray = res
+    });
 
     //this.http.get<some_type>('example.json').subscribe((res: some_type) => this.someVariable = res.json());
     console.log('get images');
   }
 
+
+  public thumbnail: string;
+  public mediaPath:string = "http://media.mw.metropolia.fi/wbma/uploads/";
+  public getThumbnail(filename){
+    return this.thumbnail = this.mediaPath + filename.split(".",1) + "-tn160.png";
+    //console.log("thumbnail: " + this.thumbnail);
+}
 
 }
